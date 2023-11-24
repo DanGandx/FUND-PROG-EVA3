eb322a729c7db1fb7837d778c8986dd92ee7e6a2
DanGandx <143562619+DanGandx@users.noreply.github.com>
2023-11-24 17:50:45 -0600

    Add files via upload

---
+++ EVA_5_REFERENCIA/src/eva_5_referencia/EVA_5_REFERENCIA.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_5_referencia;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA_5_REFERENCIA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] arreglo = new int[10];
+        System.out.println(arreglo);
+        
+    }
+    
+}
